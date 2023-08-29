b414dacd28fe27f243b3054630ba3d5f2a9e92a3
caiodna360 <caio.nogueira@dna360.ag>
2023-08-29 15:58:49 -0300

    feat: commit de configuração MADE

--- madeskinshop/assets/javascripts/pages/pages.js
+++ madeskinshop/assets/javascripts/pages/pages.js
@@ -1,9 +1,26 @@
 //addImports
 
 const Page = {
+  capturaDeLeads: function () {
+    $('[data-webform]').on('submit', function (event) {      
+      const form = $('[data-webform=leads]');
+
+      event.preventDefault();
+      
+      const honeyPot = $(this).find('[name="vnda"]');
+
+      if (honeyPot.length > 0) {
+        if (honeyPot.val() != '') {
+          console.info('ROBOT DETECTED');
+          return false;
+        }
+      }
+    });
+  },
   init: function () {
     var _this = this;
     //calls
+    _this.capturaDeLeads();
   },
 };
 
